2f1644eadb79dfd2aa3be1865e76f37ad1d9dc7c
Andres Soret <andressocha230@gmail.com>
2023-12-15 23:05:58 -0500

    Clase #22 - Desplegando el proyecto

--- src/assets/main.js
+++ src/assets/main.js
@@ -24,7 +24,7 @@ async function fetchData(urlApi) {
       <div class="group relative">
         <div
           class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
-          <img src="${video.snippet.thumbnail.high.url}" alt="${video.snippet.description}" class="w-full">
+          <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
         </div>
         <div class="mt-4 flex justify-between">
           <h3 class="text-sm text-gray-700">
@@ -33,9 +33,10 @@ async function fetchData(urlApi) {
           </h3>
         </div>
       </div>
-    `).slice(0,4).join('')}
+    `).slice(0, 4).join('')}
     `;
-  } catch {
-
+    content.innerHTML = view;
+  } catch (error) {
+    console.log(error);
   }
 })();
